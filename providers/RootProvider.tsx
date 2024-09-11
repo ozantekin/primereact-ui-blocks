1d1dbbe45b20f5aed3955f8aa5bef3f8d56f9ea1
Ozan Tekin <ardilozantekin@gmail.com>
2024-09-12 01:52:27 +0300

    feat(providers): add root provider and config

---
+++ providers/RootProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { PrimeReactProvider } from "primereact/api";
+
+import { ChildContainerProps } from "@/pr/types";
+
+import "../assets/styles/globals.css";
+import { RootContext } from "./context/RootContext";
+
+import "primeicons/primeicons.css";
+import "primereact/resources/primereact.css";
+import "primereact/resources/themes/lara-light-blue/theme.css";
+
+export default function RootProvider({ children }: ChildContainerProps) {
+  return (
+    <PrimeReactProvider>
+      <RootContext>{children}</RootContext>
+    </PrimeReactProvider>
+  );
+}
