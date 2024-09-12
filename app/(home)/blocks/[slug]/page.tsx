bd14e05ed542ec5f1aa2e12e1154a0578839bde1
Ozan Tekin <ardilozantekin@gmail.com>
2024-09-12 10:07:16 +0300

    feat(app): add home page and slug page

--- app/(home)/blocks/[slug]/page.tsx
+++ app/(home)/blocks/[slug]/page.tsx
@@ -3,33 +3,34 @@ import { allBlocks } from "contentlayer/generated";
 import { Mdx } from "@/pr/components/mdx";
 
 export const generateStaticParams = async () =>
-  allBlocks.map((post) => ({ slug: post._raw.flattenedPath }));
+  allBlocks.map((block) => ({ slug: block._raw.flattenedPath }));
 
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
-  const post = allBlocks.find(
-    (post) => post._raw.flattenedPath === params.slug
+  const block = allBlocks.find(
+    (block) => block._raw.flattenedPath === params.slug
   );
-  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
-  return { title: post.title };
+  if (!block) throw new Error(`Post not found for slug: ${params.slug}`);
+  return { title: block.title };
 };
 
-const PostLayout = ({ params }: { params: { slug: string } }) => {
-  const post = allBlocks.find(
-    (post) => post._raw.flattenedPath === params.slug
+const Blocks = ({ params }: { params: { slug: string } }) => {
+  const block = allBlocks.find(
+    (block) => block._raw.flattenedPath === params.slug
   );
-  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
+  if (!block) throw new Error(`Post not found for slug: ${params.slug}`);
 
   return (
-    <article className="mx-auto max-w-xl py-8">
-      <div className="mb-8 text-center">
-        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
-          {format(parseISO(post.date), "LLLL d, yyyy")}
+    <article className="space-y-2">
+      <div className="mb-10">
+        <time dateTime={block.date} className="mb-1 text-slate-600 text-sm">
+          {format(parseISO(block.date), "LLLL d, yyyy")}
         </time>
-        <h1 className="text-3xl font-bold">{post.title}</h1>
+        <h1 className="text-3xl font-bold">{block.title}</h1>
       </div>
-      <Mdx code={post.body.code} />
+
+      <Mdx code={block.body.code} />
     </article>
   );
 };
 
-export default PostLayout;
+export default Blocks;
