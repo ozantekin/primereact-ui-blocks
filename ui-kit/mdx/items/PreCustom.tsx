8e58b103a78820468df9713ccd907a7e7ecaee3f
Ozan Tekin <ardilozantekin@gmail.com>
2024-09-12 22:04:27 +0300

    feat(ui-kit): add ui-kit folder

---
+++ ui-kit/mdx/items/PreCustom.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button } from "primereact/button";
+
+interface PreProps extends React.HTMLProps<HTMLPreElement> {
+  __rawString__?: string;
+}
+
+export function PreCustom(props: PreProps) {
+  const { children, __rawString__ = "" } = props;
+
+  const [isCopied, setIsCopied] = React.useState(false);
+
+  const copy = async () => {
+    await navigator.clipboard.writeText(__rawString__);
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 700);
+  };
+
+  return (
+    <pre
+      className={`rounded-xl relative overflow-hidden p-[0.5rem] shadow-smooth bg-slate-100`}
+      {...props}
+    >
+      <Button
+        className="absolute right-3 top-3"
+        size="small"
+        icon={isCopied ? "pi pi-check" : "pi pi-copy"}
+        onClick={copy}
+        disabled={isCopied}
+        aria-label="Copy"
+      />
+
+      {children}
+    </pre>
+  );
+}
